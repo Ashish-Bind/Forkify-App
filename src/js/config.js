e02ec968d67fe8d343cf302ba8440f49f01fb083
Ashish <officialsandwork@gmail.com>
2023-06-16 21:39:27 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '145e2c0e-d7c6-4a10-9cd9-235169fd574c';
+export const MODAL_CLOSE_SEC = 2.5;
